30267bc91d60accd607c7c6428f94dddd603934c
linj14atwit <74051858+linj14atwit@users.noreply.github.com>
2024-11-08 08:39:59 -0500

    initial upload

---
+++ Socket/README.txt
@@ -0,0 +1,2 @@
+https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/
+
---
+++ Socket/server.py
@@ -0,0 +1,13 @@
+import asyncio
+from websockets.asyncio.server import broadcast, serve
+
+from app import app
+
+def login():
+    pass
+
+def send():
+    pass
+
+def recieve():
+    pass
