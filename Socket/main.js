52fa1f8b481a6ef8987c5a643c0b4740fe5dbde8
yaman9011 <126020905+yaman9011@users.noreply.github.com>
2024-11-21 09:13:24 -0500

    Update main.js

--- Socket/main.js
+++ Socket/main.js
@@ -1,16 +1,19 @@
 let messages = [];
-const message_limit = 25;
+let username = prompt("Enter a username: ");
+const message_limit = 15;
 
 function send(){
     let text = document.getElementById("textarea").value;
-    console.log(text);
+    console.log(`text`);
     // document.getElementById("tdisplay").innerHTML = text;
 
     if (messages.length >= message_limit) {
         messages.shift();
     }
-    
-    messages.push(text);
+    messages.push({
+        sender: username,
+        message_text: text
+    });
 
     display_messages();
 
@@ -22,13 +25,14 @@ function recieve(){
     
 }
 
-function display_messages(){
+function display_messages() {
     document.getElementById("display").innerHTML = ""
     for (let msg of messages) {
         let container = document.getElementById("display");
         let disp = document.createElement("div");
-        let d = document.createTextNode(msg);
+        let d = document.createTextNode(`${msg.sender}: ${msg.message_text}`);
 
+        disp.className = "message";
         disp.appendChild(d);
         container.appendChild(disp);
     }
